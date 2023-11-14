a26e543de30ae7b68f7fca272e17e71be4b3e23d
Defy <61808223+De-y@users.noreply.github.com>
2023-11-13 18:16:30 -0800

    e

--- components/projects.vue
+++ components/projects.vue
@@ -7,14 +7,13 @@
         <div v-if="projectData.status.value == 'pending'">
             <h1>Loading...</h1>
         </div>
-        <!-- Basically we are getting all the project information from the projects api and then displaying them all. -->
+        <!-- Basically we are getting all the project information from the projects api (JSON FILE) and then displaying them all. -->
         <div v-for = "project in projectData.data.value?.data.projects" :key="project.id" class="carousel">
-            <div class='project-card'>
-                <h2>{{ project.name }}</h2>
-                <p>{{ project.description }}</p>
-                <p>Technologies Used: </p>
-                <p v-for="technologies in project.technologies">{{ technologies }}</p>
-                <a :href="project.url">Link</a>
+            <div class='project-card' :style="{ backgroundImage: `url(${project.image})` }">
+                <div class="description">
+                    <h2>{{ project.name }}</h2>
+                    <a :href="project.url"><strong>Link</strong></a>
+                </div>
             </div>
         </div>
     </div>
@@ -41,10 +40,33 @@
         padding: 10px;
         margin: 10px;
         text-align: center;
+        width: 350px;
+        height: 250px;
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: multiply;   
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;     
+    }
+    .project-card .description {
+        font-size: 20px;
+        width: 50%;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        transform: translateY(50%);        
     }
     .project-card a {
         text-decoration: none;
         color: rgb(0, 255, 221);
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 20px;
+    }
+    .project-card a:hover {
+        color: rgb(0, 190, 204);
+        transition: 500ms;
+
     }
     .project-explorer {
         color: rgb(255, 0, 0);
